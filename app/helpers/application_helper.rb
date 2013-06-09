54c3284a3bce3e2e760a72dd496d75f5abfda0e5
Rainer Weitz <rweitz@fulcrum.net>
2013-06-08 22:55:56 -0700

    Moved parse_oracle_error function to application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def parse_oracle_error(message)
+    start_position = message.index("ORA-20000:") + 10
+    end_position = message.index("ORA-",start_position+1) - 1
+    message[start_position..end_position]
+  end
 end
