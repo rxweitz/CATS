df8abeae4b39f2ae20a724b874474de2c86eb595
Rainer Weitz <rweitz@fulcrum.net>
2013-06-09 00:05:00 -0700

    Added validation to prevent Oracle errors

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -8,4 +8,17 @@ class Condition < ActiveRecord::Base
   set_primary_key 'conditionid'
   set_sequence_name 'cats_conditionid'
   attr_accessible :conditioncode, :description
+
+  before_validation do
+    self.conditioncode.upcase!
+    self.description.upcase!
+  end
+
+  # the conditioncode and description are mandatory
+  validates_presence_of :conditioncode
+  validates_presence_of :description
+
+  # conditioncode must be unique
+  validates_uniqueness_of :conditioncode
+
 end
